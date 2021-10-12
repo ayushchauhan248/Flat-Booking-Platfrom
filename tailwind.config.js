9a2f4088f8bd6190cf1bb0367fb77c1c466ea623
khushiv30 <khushi.varshney_cs19@gla.ac.in>
2021-10-12 22:30:13 +0530

    added colour code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor:{
+        yellow:{
+          DEFAULT:'#b69100'
+        }
+      },
+      backgroundColor:theme =>({
+        'primary':'#b69100',
+      })
+    },
   },
   variants: {
     extend: {},
