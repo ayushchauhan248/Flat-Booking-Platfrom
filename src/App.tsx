36f8a50abd7ec111cb1a2621e33166ad891c07da
khushiv30 <khushi.varshney_cs19@gla.ac.in>
2021-10-12 22:30:41 +0530

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
 
+import Footer from './Components/HomeComponents/Footer';
+import Navbar from './Components/HomeComponents/Navbar';
+import Card from './Components/HomeComponents/Card';
+import Overview from './Components/HomeComponents/Overview';
+import Card2 from './Components/HomeComponents/Card2';
 function App() {
     return (
         <div>
@@ -15,9 +20,29 @@ function App() {
                     <Route path="/login">
                         <LoginPage></LoginPage>
                     </Route>
+                    <Route path="/navbar">
+                        <Navbar></Navbar>
+                        
+                    </Route>
                     <Route path="/signup">
                         <SignupPage></SignupPage>
                     </Route>
+                    <Route path="/footer">
+                        <Footer></Footer>
+                    </Route>
+                    <Route path="/card">
+                        <Card></Card>
+                    </Route>
+                   
+                    <Route path="/overview">
+                        <Overview></Overview>
+                    </Route>
+
+                    <Route path="/card2">
+                        <Card2></Card2>
+                    </Route>
+                    
+                
                 </Switch>
             </BrowserRouter>
         </div>
