a8ea84c6f6f5ce1a13849c18c895692b10b2d0eb
khushiv30 <khushi.varshney_cs19@gla.ac.in>
2021-10-12 22:33:20 +0530

    added links

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -8,6 +8,11 @@ return (
  <div>This is Home Page</div>
  <Link to="/signup"><span className=" text-blue-500"> Signup</span> </Link>
  <Link to="/login"><span className=" text-blue-500"> Login</span> </Link>
+ <Link to="/navbar"><span className=" text-blue-500"> Navbar</span> </Link>
+ <Link to="/footer"><span className=" text-blue-500"> Footer</span> </Link>
+ <Link to="/card"><span className=" text-blue-500"> Card</span> </Link>
+ <Link to="/overview"><span className=" text-blue-500"> Overview</span> </Link>
+ <Link to="/card2"><span className=" text-blue-500"> Card2</span> </Link>
  </>
 );
 };
